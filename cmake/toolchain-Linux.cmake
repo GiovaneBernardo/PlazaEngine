953a0aeab3b0f15b32da2382833822bec5d8aaea
GiovaneBernardo <souzagiovane2019@gmail.com>
2025-03-18 11:57:14 -0300

    Changed the way to add flags on linux toolchain file

--- cmake/toolchain-Linux.cmake
+++ cmake/toolchain-Linux.cmake
@@ -1,15 +1,8 @@
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_COMPILER clang++)
 
-add_compile_options(
-	-Wno-unsafe-buffer-usage
-	-fsafe-buffer-usage-suggestions
-	-Wno-switch-default
-	-Wswitch
-	-Wunused-value
-	-Wno-invalid-offsetof
-	-fdeclspec
-	-fms-extensions
-	-Wno-unused-but-set-variable
-	-O2
-)
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-unsafe-buffer-usage -fsafe-buffer-usage-suggestions -Wno-switch-default -Wswitch -Wunused-value -Wno-invalid-offsetof -fdeclspec -fms-extensions -Wno-unused-but-set-variable")
+
+if(CMAKE_BUILD_TYPE STREQUAL "Release")
+	set(CMAKE_CXX_FLAGS_RELEASE "/O2")
+endif()
