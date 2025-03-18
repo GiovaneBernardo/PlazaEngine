8d58ee140938ba4bac32c4bbc922a01c137ac277
GiovaneBernardo <souzagiovane2019@gmail.com>
2025-03-18 12:34:09 -0300

    Added clang error supressors for release builds too

--- cmake/toolchain-Linux.cmake
+++ cmake/toolchain-Linux.cmake
@@ -4,5 +4,5 @@ set(CMAKE_CXX_COMPILER clang++)
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-unsafe-buffer-usage -fsafe-buffer-usage-suggestions -Wno-switch-default -Wswitch -Wunused-value -Wno-invalid-offsetof -fdeclspec -fms-extensions -Wno-unused-but-set-variable")
 
 if(CMAKE_BUILD_TYPE STREQUAL "Release")
-	set(CMAKE_CXX_FLAGS_RELEASE "/O2")
+	set(CMAKE_CXX_FLAGS_RELEASE "/O2 ${CMAKE_CXX_FLAGS}")
 endif()
