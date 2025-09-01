b61f7cb8ca0ea83721778d95ae00669340c5e485
GiovaneBernardo <souzagiovane2019@gmail.com>
2025-08-31 23:29:44 -0300

    Improve in-editor console to show colored text

--- Editor/GUI/Console/Console.cpp
+++ Editor/GUI/Console/Console.cpp
@@ -25,10 +25,40 @@ namespace Plaza {
 				ImGui::Checkbox("Update Indirect Instances", &mTemporaryVariables.updateIndirectInstances);
 			}
 
-			for (const LogMessage& message : Plaza::Log::GetVectorSink()->GetLogs()) {
-				ImGui::Text(message.mMessage.c_str());
-				ImGui::SameLine();
-				ImGui::Text(ctime(&message.mTime));
+			if (ImGui::BeginTable("ConsoleTable", 2,
+					ImGuiTableFlags_RowBg | ImGuiTableFlags_BordersInnerV | ImGuiTableFlags_ScrollY))
+			{
+				ImGui::TableSetupColumn("Message", ImGuiTableColumnFlags_WidthStretch);
+				ImGui::TableSetupColumn("Time", ImGuiTableColumnFlags_WidthFixed, 120.0f);
+
+				int index = 0;
+				for (const LogMessage& message : Plaza::Log::GetVectorSink()->GetLogs()) {
+					ImGui::TableNextRow();
+
+					// Message column
+					ImGui::TableSetColumnIndex(0);
+
+					ImVec4 color;
+					switch (message.mLevel) {
+						case spdlog::level::trace:    color = ImVec4(0.6f, 0.6f, 0.6f, 1.0f); break;
+						case spdlog::level::debug:    color = ImVec4(0.4f, 0.8f, 1.0f, 1.0f); break;
+						case spdlog::level::info:     color = ImVec4(1.0f, 1.0f, 1.0f, 1.0f); break;
+						case spdlog::level::warn:     color = ImVec4(1.0f, 1.0f, 0.0f, 1.0f); break;
+						case spdlog::level::err:      color = ImVec4(1.0f, 0.2f, 0.2f, 1.0f); break;
+						case spdlog::level::critical: color = ImVec4(1.0f, 0.0f, 0.0f, 1.0f); break;
+						default:                      color = ImVec4(1.0f, 1.0f, 1.0f, 1.0f); break;
+					}
+
+					ImGui::TextColored(color, "%s", message.mMessage.c_str());
+
+					// Time column
+					ImGui::TableSetColumnIndex(1);
+					ImGui::TextDisabled("%s", ctime(&message.mTime));
+
+					index++;
+				}
+
+				ImGui::EndTable();
 			}
 
 			if (ImGui::InputText("##ConsoleInput", mConsoleInput, sizeof(mConsoleInput),
