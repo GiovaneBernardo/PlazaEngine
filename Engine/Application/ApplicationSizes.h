13c1b2d24763d8bf44ced881f49957a60020a4b6
GiovaneBernardo <souzagiovane2019@gmail.com>
2026-02-02 01:31:04 -0300

    Increase size on ApplicationSizes temporarily

--- Engine/Application/ApplicationSizes.h
+++ Engine/Application/ApplicationSizes.h
@@ -6,7 +6,7 @@ namespace Plaza {
 		float appHeaderSize = 50.0f; // The header size above scene view
 		glm::vec2 baseAppSize = {1920, 1080};
 		glm::vec2 appSize = {2560, 1080};
-		glm::vec2 sceneSize = {1820.0f, 720.0f};
+		glm::vec2 sceneSize = {1920.0f, 1080.0f};
 		glm::vec2 sceneStart = {0.0f, 0.0f};
 		glm::vec2 hierarchySize = {320.0f, 1000.0f};
 		glm::vec2 inspectorSize = {420.0f, 1000.0f};
